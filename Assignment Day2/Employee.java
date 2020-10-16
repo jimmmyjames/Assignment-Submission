35783dbe9c3fe7afce0c686b9debc269c2d5ff9f
jimmmyjames <72152646+jimmmyjames@users.noreply.github.com>
2020-10-16 22:35:42 +0530

    Day2 Assignment Submitted

---
+++ Assignment Day2/Employee.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class Employee {
+    String name = "Saurabh";
+    int age = 23;
+    String city = "Chennai";
+    public void display(){
+        System.out.println("The name is " + name);
+        System.out.println("The age is " + age);
+        System.out.println("The city is " + city);
+    }
+}
---
+++ Assignment Day2/Main.java
@@ -0,0 +1,11 @@
+package com.java;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee a = new Employee();
+        a.display();
+        Employee b = new Employee();
+        b.display();
+    }
+
+}
